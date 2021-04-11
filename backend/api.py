e2bfc0c9d78942a95b5be3d0360f751b5dd04144
Ubuntu <ubuntu@ip-172-31-24-49.ec2.internal>
2021-04-11 21:10:02 +0000

    Folder Fix

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from createUser import init
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    path = init()
+    return 'Hello, World!: '+str(path)
--- backend/createUser.py
+++ backend/createUser.py
@@ -5,12 +5,11 @@ import uuid
 
 def init():
     client_id = uuid.uuid4()
-    os_dir = os.getcwd()
     #Create Folders if programm folder not exists
-    Path(f"{os_dir}\\Clients\\").mkdir(parents=True, exist_ok=True)
-    Path(f"{os_dir}\\Clients\\"+str(client_id)).mkdir(parents=True, exist_ok=True)
-    Path(f"{os_dir}\\Clients\\"+str(client_id)+"\\Input").mkdir(parents=True, exist_ok=True)
-    Path(f"{os_dir}\\Clients\\"+str(client_id)+"\\processed_Input").mkdir(parents=True, exist_ok=True)
+    Path("Clients").mkdir(parents=True, exist_ok=True)
+    Path("Clients/"+str(client_id)).mkdir(parents=True, exist_ok=True)
+    Path("Clients/"+str(client_id)+"/Input").mkdir(parents=True, exist_ok=True)
+    Path("Clients/"+str(client_id)+"/processed_Input").mkdir(parents=True, exist_ok=True)
 
-    path_to_soundfilefolder = f"{os_dir}\\Clients\\"+str(client_id)+"\\Input"
+    path_to_soundfilefolder = "Clients/"+str(client_id)+"/Input"
     return path_to_soundfilefolder
