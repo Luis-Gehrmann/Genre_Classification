ceaf2e89d2d849f4682ccb3ece3766627a02e98f
xXD4rkM4tt3rXx <50627528+xXD4rkM4tt3rXx@users.noreply.github.com>
2021-04-12 02:18:50 +0200

    Update convert.py

--- backend/convert.py
+++ backend/convert.py
@@ -6,10 +6,10 @@ def convertMP3(path):
     for i in os.listdir(path_to_soundfilefolder):
         end = i.split(".")[-1]
         if end == "mp3":
-            src = f"{path_to_soundfilefolder}\\{i}"
+            src = f"{path_to_soundfilefolder}/{i}"
             sound = AudioSegment.from_mp3()
             i = i.split(".mp3")[0]
-            dst = f"{path_to_soundfilefolder}\\{i}.wav"
+            dst = f"{path_to_soundfilefolder}/{i}.wav"
             sound.export(dst, format="wav")
         else:
             print("No MP3 found")
