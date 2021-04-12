2e976e7dae4c7b659f3f90a533c8aeac403080d3
LemonLuis <52485264+Luis-Gehrmann@users.noreply.github.com>
2021-04-12 02:03:49 +0200

    Update convert.py

--- backend/convert.py
+++ backend/convert.py
@@ -1,12 +1,13 @@
 import os
 from pydub import AudioSegment
 
-def convertMP3(path):
-    path_to_soundfilefolder = path
-    for i in os.listdir(path_to_soundfilefolder):
-        if ".mp3" in i:
-            src = f"{path_to_soundfilefolder}\\{i}"
-            sound = AudioSegment.from_mp3()
-            i = i.split(".mp3")[0]
-            dst = f"{path_to_soundfilefolder}\\{i}.wav"
-            sound.export(dst, format="wav")
+os_dir = os.getcwd()
+path_to_soundfilefolder = f"{os_dir}\\test"
+for i in os.listdir(path_to_soundfilefolder):
+    end = i.split(".")[-1]
+    if end == "mp3":
+        src = f"{path_to_soundfilefolder}\\{i}"
+        sound = AudioSegment.from_mp3()
+        i = i.split(".mp3")[0]
+        dst = f"{path_to_soundfilefolder}\\{i}.wav"
+        sound.export(dst, format="wav")
