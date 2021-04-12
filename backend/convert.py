1e51899106e9bc6b817daf8087e75a9f6560a1ce
xXD4rkM4tt3rXx <50627528+xXD4rkM4tt3rXx@users.noreply.github.com>
2021-04-12 02:04:29 +0200

    Update convert.py

--- backend/convert.py
+++ backend/convert.py
@@ -11,3 +11,5 @@ for i in os.listdir(path_to_soundfilefolder):
         i = i.split(".mp3")[0]
         dst = f"{path_to_soundfilefolder}\\{i}.wav"
         sound.export(dst, format="wav")
+    else:
+        print("No MP3 found")
